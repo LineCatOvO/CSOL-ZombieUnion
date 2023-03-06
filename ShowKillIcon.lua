85894d07e4fdd0c13fadf79b54ba25f140a6a52d
LineCatOvO <1501341002@qq.com>
2023-03-06 09:33:27 +0000

    初步编写了ShowKillIcon的大体函数内容
    Signed-off-by: LineCatOvO <1501341002@qq.com>

--- ShowKillIcon.lua
+++ ShowKillIcon.lua
@@ -9,11 +9,16 @@ local Kill1Icon="配置全部box的数据的表"
 local Kill2Icon="配置全部box的数据的表"
 local Kill3Icon="配置全部box的数据的表"
 local Kill4Icon="配置全部box的数据的表"
+local FadeOutNum=2--控制Kill图标淡出速度
 ShowKillTimePeriod=0
-function UpdateRefreshKillIcon()
-    for i=1,512 do
-        if KillBoxTable[i]:Get(a)!=0 then
-            if
+function UpdateRefreshKillIcon()--如果有正在显示的box，那么就逐渐把透明度降低为0。
+    for i=1,#KillBoxTable do
+    local mya=KillBoxTable[i]:Get(a)
+        if mya!=0 then
+            if UITime-ShowKillTimePeriod>0.05 then
+                KillBoxTable[i]:Set({a=mya-FadeOutNum})
+                ShowKillTimePeriod=UITime
+            end
         end
     end
 end
