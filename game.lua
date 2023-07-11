f7aeba07d6ddf398ad182d63246adf6345fd0610
LineCatOvO <10994461+linecatovo@user.noreply.gitee.com>
2023-07-11 11:59:18 +0800

    新增了一个计划，将更改移速和困住玩家整合进一个函数

--- game.lua
+++ game.lua
@@ -2,70 +2,98 @@
 
 --Author：LineCatOvO
 print("game" .. "已加载")
-AuthorDebug = false       --DEBUG模式开关，开启则输出控制台信息
+AuthorDebug = false         --DEBUG模式开关，开启则输出控制台信息
 Game.Rule.breakable = false --地图是否可破坏
-SelfDamage = false        --对自己的伤害是否有效
-AccidentDamage = false    --是否计算意外伤害
-Skill5 = {}               --5技能时间刻(时间/玩家名)
-Skill6 = {}               --6技能时间刻(时间/玩家名)
-SkillG = {}               --G技能时间刻(时间/玩家名)
-Skill5StartTime = {}      --5技能开始时间(时间/玩家名)
-Skill6StartTime = {}      --6技能开始时间(时间/玩家名)
-SkillGStartTime = {}      --7技能开始时间(时间/玩家名)
+SelfDamage = false          --对自己的伤害是否有效
+AccidentDamage = false      --是否计算意外伤害
 CircleDamage = "CircleDamage"
 GroupHealthRestore = "HealthRestore"
 ShockWave = "ShockWave"
 WantChange = false
 TimeExpired = false
-SpawnTime = {}
-Skill5Time = 15       --5技能持续时间
-Skill5BadTime = 4     --5技能DEBUFF时间
-Skill6Time = 10       --6技能时间
+Skill5Time = 15         --5技能持续时间
+Skill5BadTime = 4       --5技能DEBUFF时间
+Skill6Time = 10         --6技能时间
 Skill5CoolDownTime = 45 --5技能冷却时间
 Skill6CoolDownTime = 60 --6技能冷却时间
 SkillGCoolDownTime = {} --各个僵尸的技能冷却时间表
 SkillGCoolDownTime[Game.MODEL.NORMAL_ZOMBIE] = 15
 SkillGCoolDownTime[Game.MODEL.LIGHT_ZOMBIE] = 30
 SkillGCoolDownTime[Game.MODEL.HEAVY_ZOMBIE] = 30
-SkillGTime = {} --各个僵尸的技能持续时间表
 SkillGTime[Game.MODEL.NORMAL_ZOMBIE] = 8
 SkillGTime[Game.MODEL.LIGHT_ZOMBIE] = 8
 SkillGTime[Game.MODEL.HEAVY_ZOMBIE] = 0
 
---[[SkillGList={}--僵尸技能表（已废弃）
-SkillGList[Game.MODEL.NORMAL_ZOMBIE]={}
-SkillGList[Game.MODEL.NORMAL_ZOMBIE][G]=ZombieSpeedBoost
-SkillGList[Game.MODEL.LIGHT_ZOMBIE]={}
-SkillGList[Game.MODEL.LIGHT_ZOMBIE][G]=LightZombieSpeedBoost
-SkillGList[Game.MODEL.HEAVY_ZOMBIE]={}
-SkillGList[Game.MODEL.HEAVY_ZOMBIE][G]=Trap
-SkillGList[Game.MODEL.PHYCHO_ZOMBIE]={}
-SkillGList[Game.MODEL.PHYCHO_ZOMBIE][G]=CircleDamage
-SkillGList[Game.MODEL.VOODOO_ZOMBIE]={}
-SkillGList[Game.MODEL.VOODOO_ZOMBIE][G]=GroupHealthRestore
-SkillGList[Game.MODEL.DEIMOS_ZOMBIE]={}
-SkillGList[Game.MODEL.DEIMOS_ZOMBIE][G]=ShockWave
-SkillGList[Game.MODEL.GANYMEDE_ZOMBIE]={}
-SkillGList[Game.MODEL.STAMPER_ZOMBIE]={}
-SkillGList[Game.MODEL.BANSHEE_ZOMBIE]={}
-SkillGList[Game.MODEL.VENOMGUARD_ZOMBIE]={}
-SkillGList[Game.MODEL.STINGFINGER_ZOMBIE]={}
-SkillGList[Game.MODEL.METATRON_ZOMBIE]={}
-SkillGList[Game.MODEL.LILITH_ZOMBIE]={}
-SkillGList[Game.MODEL.CHASER_ZOMBIE]={}
-SkillGList[Game.MODEL.BLOTTER_ZOMBIE]={}
-SkillGList[Game.MODEL.RUSTYWING_ZOMBIE]={}
-SkillGList[Game.MODEL.AKSHA_ZOMBIE]={}]]
-                                         --
-LastTime = {}                            --上一次Update结束时间
-GameTime = 0                             --当前游戏时间
+--玩家信息集合(一般格式为:时间/玩家名)
+SkillGTime = {}      --各个僵尸的技能持续时间表
+SpawnTime = {}       --各个玩家的重生时间刻
+LastTime = {}        --上一次Update结束时间
+Skill5 = {}          --5技能时间刻(时间/玩家名)
+Skill6 = {}          --6技能时间刻(时间/玩家名)
+SkillG = {}          --G技能时间刻(时间/玩家名)
+Skill5StartTime = {} --5技能开始时间(时间/玩家名)
+Skill6StartTime = {} --6技能开始时间(时间/玩家名)
+SkillGStartTime = {} --7技能开始时间(时间/玩家名)
+
+GameTime = 0         --当前游戏时间
 TDM = Game.Rule
 TDM.name = "團隊死鬥"
 TDM.desc = "使用Lua制作的團隊死鬥模式"
---僵尸技能表
+--文字表
 G = "G"
 DamageEnhance = "DamageEnhance"
 SpeedBoost = "SpeedBoost"
+
+
+--To do:使用统一的表（PlayerEffectList）和方法(AttachEffect),将移速更改和禁止移动整合到一起
+PlayerEffectList = {}
+function AttachEffect(effect, player, effectInfo)
+    function checkAvaliable(e, info) --effect,effectInfo,检查信息是否无误，是否无冲突
+        local functionTable = {
+            ["ChangeSpeed"] = function()
+                if (PlayerEffectList[player.name] and PlayerEffectList[player.name].effect == "ChangeSpeed")
+                then
+                    if (PlayerEffectList[player.name].allowOverride)
+                    then
+                        return false
+                    end
+                end
+            end,
+            ["TrapPlayer"] = function()
+
+            end
+        }
+        return true
+    end
+
+    function AddEffectRequest(e, p, info) --effect,player,effectInfo
+        local functionTable = {
+            ["ChangeSpeed"] = function()
+                --speed,allowOverride,effect,time
+                --speed：限制范围
+                --allowOverride：0：禁止；1：允许；2：允许更高；3：允许更低；
+                --effect：效果名字，是什么照着写就行
+                --time：>=0：正常计算时长；-1：无限时长
+            end,
+            ["TrapPlayer"] = function()
+
+            end
+        }
+    end
+
+    local functionTable = {
+        ["ChangeSpeed"] = function()
+            --先检查信息是否无误，是否无冲突，整合进checkAvaliable
+            if (checkAvaliable(effect, effectInfo)) then
+                AddEffectRequest(effect, player, effectInfo)
+            end
+        end,
+        ["TrapPlayer"] = function()
+
+        end
+    }
+end
+
 SpeedList = {}
 function ChangeSpeed(player, speed, time, reset) --申请移速更改（僵尸不能改移速，这个基本废了）
     if SpeedList[player.name] ~= nil then
@@ -105,17 +133,17 @@ function UpdateChangeSpeed(player) --实时刷新玩家速度的函数
     end
 end
 
-TrapList = {}                                 --“鬼手”技能对象列表
+TrapList = {} --“鬼手”技能对象列表
 function Trap(Me)
     --使用鬼手技能的函数（大肥：拉个最近的人直接出鬼手，锁他三秒）
     local NearestPlayer = Game.Player:Create(1) --存储最近的玩家的变量
-    if NearestPlayer == Me then               --如果这个玩家是自己
-        NearestPlayer = Game.Player:Create(2) --那就换一个
+    if NearestPlayer == Me then                 --如果这个玩家是自己
+        NearestPlayer = Game.Player:Create(2)   --那就换一个
     end
-    for i = 1, 24 do                          --标准的遍历比较
-        local player = Game.Player:Create(i)  --选一个人
+    for i = 1, 24 do                            --标准的遍历比较
+        local player = Game.Player:Create(i)    --选一个人
         if player ~= player and player ~= nil then
-            local NowDistance =               --目前的最近距离
+            local NowDistance =                 --目前的最近距离
                 ((NearestPlayer.position.x - Me.position.x) ^ 2
                     + (NearestPlayer.position.y - Me.position.y) ^ 2
                     + (NearestPlayer.position.z - Me.position.z) ^ 2) ^ 0.5
@@ -124,22 +152,22 @@ function Trap(Me)
                     + (player.position.y - Me.position.y) ^ 2
                     + (player.position.z - Me.position.z) ^ 2) ^ 0.5
             if NowDistance > SelectedDistance then --如果新的比旧的近
-                NearestPlayer = player           --把选择的玩家换成新的
-            end                                  --最后就能遍历出最近的玩家了
+                NearestPlayer = player             --把选择的玩家换成新的
+            end                                    --最后就能遍历出最近的玩家了
         end
     end
     if NearestPlayer == nil or (((NearestPlayer.position.x - Me.position.x) ^ 2
             + (NearestPlayer.position.y - Me.position.y) ^ 2
             + (NearestPlayer.position.z - Me.position.z) ^ 2) ^ 0.5) > 13 then
         return false
-    end                                                                        --距离超过13则不工作
+    end --距离超过13则不工作
     --把最近的人困住五秒
     SkillG[Me.name] = 0
     SkillGStartTime[Me.name] = GameTime
-    TrapList[Me.name] = {                   --Me是执行者
-        victimname = NearestPlayer.name,    --victim是受害者
+    TrapList[Me.name] = {                 --Me是执行者
+        victimname = NearestPlayer.name,  --victim是受害者
         time = 5,
-        position = NearestPlayer.position   --离得最近的倒霉蛋的位置
+        position = NearestPlayer.position --离得最近的倒霉蛋的位置
     }
     return true
 end
@@ -267,7 +295,7 @@ function PerformSkill(skill, player)
     end
 end
 
-function IfSkillActive(skill, player)
+function IfSkillActive(skill, player) --返回技能是否有效
     if player ~= nil then
         if skill == 5 and Skill5[player.name] ~= nil then
             if Skill5[player.name] ~= -1 and Skill5[player.name] < Skill5Time then
@@ -311,7 +339,7 @@ function UpdateShowSkillByArmor(player)
                 FirstStatus = active
             else
                 local percentage, fl = math.modf((ready5 - notready) * (Skill5[player.name] - Skill5Time) /
-                (Skill5CoolDownTime))
+                    (Skill5CoolDownTime))
                 FirstStatus = notready + percentage
             end
             if Skill6[player.name] == -1 then
@@ -320,7 +348,7 @@ function UpdateShowSkillByArmor(player)
                 SecondStatus = active
             else
                 local percentage, fl = math.modf((ready6 - notready) * (Skill6[player.name] - Skill6Time) /
-                (Skill6CoolDownTime))
+                    (Skill6CoolDownTime))
                 SecondStatus = notready + percentage
             end
             --[[else
@@ -331,13 +359,13 @@ function UpdateShowSkillByArmor(player)
                 else
                     FirstStatus=notready
                 end]]
-                      --
+            --
         else
             if SkillG[player.name] == -1 then
                 FirstStatus = readyG
             else
                 local percentage, fl = math.modf((readyG - notready) * (SkillG[player.name]) /
-                (SkillGCoolDownTime[player.model]))
+                    (SkillGCoolDownTime[player.model]))
                 FirstStatus = notready + percentage
             end
         end
